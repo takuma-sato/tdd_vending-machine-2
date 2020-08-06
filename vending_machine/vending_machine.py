c824cd55b36e93c6cef1ee3f6976d46b71155543
hk-tech <hirotokm1412@gmail.com>
2020-08-06 12:57:33 +0900

    fix total money amount

--- vending_machine/vending_machine.py
+++ vending_machine/vending_machine.py
@@ -1,21 +1,13 @@
 class VendingMachine:
     def __init__(self):
-        self.money_dict = {
-            "10": 0,
-            "50": 0,
-            "100": 0,
-            "500": 0,
-            "1000": 0,
-        }
+        self.total_money_amount = 0
 
-    def insert_money(self, money_type):
-        #
-        pass
+    def insert_money(self, money):
+        self.total_money_amount += money
 
     def get_total_money_amount(self):
-        # yamazaki san ga kaku
         return self.total_money_amount
 
     def refund_money(self):
-        return self.money_dict
+        return self.total_money_amount
 
