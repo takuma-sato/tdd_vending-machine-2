f54cce60051f1c970ab9dce5a442e3b30b343cd6
seiya.yamazaki <seiya.yamazaki@ZARS01noMBP.lan>
2020-08-06 14:15:21 +0900

    フィードバック付き

--- tests/test_vending_machine.py
+++ tests/test_vending_machine.py
@@ -1,11 +1,24 @@
-import unittest
-import sys
-
-sys.path.append("../")
-from vending_machine import VendingMachine
+from vending_machine.vending_machine import VendingMachine
 
 
 class TestVendingMachine:
     def test_insert_money(self):
         vm = VendingMachine()
-        self.assertEqual()
+        vm.insert_money(10)
+        vm.insert_money(1)
+        assert vm.total_money_amount == 10
+
+    def test_get_total_money_amount(self):
+        vm = VendingMachine()
+        vm.insert_money(10)
+        vm.insert_money(500)
+        vm.insert_money(1000)
+        assert vm.get_total_money_amount() == 1510
+
+    def test_refund_money(self):
+        vm = VendingMachine()
+        vm.insert_money(10)
+        vm.insert_money(500)
+        vm.insert_money(1000)
+        assert vm.refund_money() == 1510
+
--- vending_machine/vending_machine.py
+++ vending_machine/vending_machine.py
@@ -1,9 +1,17 @@
 class VendingMachine:
     def __init__(self):
         self.total_money_amount = 0
+        self.juice_dict = {
+            "コーラ": Juice(price=120, name="コーラ", stock=5)
+        }  # Juiceと本数を渡す {"cola":(Juice, stock)}
+        self.sales_amount = 0
 
     def insert_money(self, money):
-        self.total_money_amount += money
+        if money in [10, 50, 100, 500, 1000]:
+            self.total_money_amount += money
+            return
+        else:
+            return money
 
     def get_total_money_amount(self):
         return self.total_money_amount
@@ -11,3 +19,14 @@ class VendingMachine:
     def refund_money(self):
         return self.total_money_amount
 
+    def purchase(self, juice_name):
+        pass
+        # if juice_name in self.juice_dict.keys():
+        #     self.juice_dict[juice_name].stock > 0
+
+
+class Juice:
+    def __init__(self, price, name, stock) -> None:
+        self.price = price
+        self.name = name
+        self.stock = stock
