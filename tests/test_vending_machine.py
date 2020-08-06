8359a643d13486fed6185e2be026d8de2fc30704
hk-tech <hirotokm1412@gmail.com>
2020-08-06 12:54:46 +0900

    add initial functions and tests

---
+++ tests/test_vending_machine.py
@@ -0,0 +1,11 @@
+import unittest
+import sys
+
+sys.path.append("../")
+from vending_machine import VendingMachine
+
+
+class TestVendingMachine:
+    def test_insert_money(self):
+        vm = VendingMachine()
+        self.assertEqual()
---
+++ vending_machine/vending_machine.py
@@ -0,0 +1,21 @@
+class VendingMachine:
+    def __init__(self):
+        self.money_dict = {
+            "10": 0,
+            "50": 0,
+            "100": 0,
+            "500": 0,
+            "1000": 0,
+        }
+
+    def insert_money(self, money_type):
+        #
+        pass
+
+    def get_total_money_amount(self):
+        # yamazaki san ga kaku
+        return self.total_money_amount
+
+    def refund_money(self):
+        return self.money_dict
+
